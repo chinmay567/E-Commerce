3c9c165ff17cf6eccf957d9b49edd8a8e653c6db
chinmay567 <dora.chinmay567@gmail.com>
2021-05-24 20:38:21 +0530

    first commit

---
+++ static/js/cart.js
@@ -0,0 +1,60 @@
+let updateBtn = document.getElementsByClassName('update-cart')
+
+for(let i = 0;i<updateBtn.length;i++){
+    updateBtn[i].addEventListener('click',function(){
+        let productId = this.dataset.product;
+        let action = this.dataset.action;
+        console.log('productId:',productId,'action:',action);
+
+        console.log('user:',user);
+        if(user == 'AnonymousUser'){
+            addCookieItem(productId,action)
+        }
+        else{
+            updateUserOrder(productId,action)
+        }
+    })
+}
+
+function addCookieItem(productId,action) {
+    console.log("user is not logged in");
+    if (action == 'add') {
+        if (cart[productId] == undefined) {
+            cart[productId] = {'quantity':1}
+        }else{
+            cart[productId]['quantity'] += 1 
+        }
+    }
+    if (action == 'remove') {
+        cart[productId]['quantity'] -= 1
+        if (cart[productId]['quantity'] <= 0) {
+            console.log('remove Item');
+            delete cart[productId]
+        }
+    }
+    console.log('Cart:',cart);
+    document.cookie = 'cart=' + JSON.stringify(cart) + ";domain=;path=/"
+    location.reload() 
+}
+
+function updateUserOrder(productId,action) {
+    console.log("user is logged in");
+
+    let url = '/update_item/'
+
+    fetch(url,{
+        method:'POST',
+        headers:{
+            'Content-Type' :'application/json',
+            'X-CSRFToken':csrftoken,
+        },
+        body:JSON.stringify({'productId':productId,'action':action})
+    })
+    .then((response)=>{
+        return response.json()
+    })
+    .then((data)=>{
+        console.log('data:',data);
+        location.reload()
+    })
+}
